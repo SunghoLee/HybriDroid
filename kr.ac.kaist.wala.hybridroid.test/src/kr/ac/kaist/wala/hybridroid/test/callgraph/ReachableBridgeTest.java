25aca5c1e0de62e219b52083e15380e8bffea259
LeeSungho <leesh@LeeSunghoui-iMac.local>
2016-07-21 14:29:57 +0900

    Implement two more tests for call graph reachability

--- kr.ac.kaist.wala.hybridroid.test/src/kr/ac/kaist/wala/hybridroid/test/callgraph/ReachableBridgeTest.java
+++ kr.ac.kaist.wala.hybridroid.test/src/kr/ac/kaist/wala/hybridroid/test/callgraph/ReachableBridgeTest.java
@@ -1,6 +1,7 @@
 package kr.ac.kaist.wala.hybridroid.test.callgraph;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -34,7 +35,8 @@ public class ReachableBridgeTest {
 	
 	@Test
 	public void haveEdgesToReachableJavaMethodFromJavaScript() throws ClassHierarchyException, IllegalArgumentException, IOException, CancelException{
-		File[] tests = FileCollector.getAPKsInDir(HybriDroidTestRunner.getTestDir() + File.separator + TEST_DIR);
+//		File[] tests = FileCollector.getAPKsInDir(HybriDroidTestRunner.getTestDir() + File.separator + TEST_DIR);
+		File[] tests = {new File(HybriDroidTestRunner.getTestDir() + File.separator + TEST_DIR + File.separator + "ReachableMethodTest.apk")};
 		
 		for(File f : tests){
 			String testName = f.getName();
@@ -56,19 +58,167 @@ public class ReachableBridgeTest {
 			    IMethod getM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(bridgeC.getReference(), Selector.make("getName()Ljava/lang/String;")));
 			    CGNode getNode = cg.getNode(getM, Everywhere.EVERYWHERE);
 			    			   		    
-			    assertNotNull("No bridge init method", initNode);
-			    assertNotNull("No bridge send method", sendNode);
-			    assertNotNull("No bridge get method", getNode);
+			    assertNotNull("No bridge init method.", initNode);
+			    assertNotNull("No bridge send method.", sendNode);
+			    assertNotNull("No bridge get method.", getNode);
 				
-			    assertTrue("Only one successor is needed for init method", cg.getSuccNodeCount(initNode) == 1);
-			    assertTrue("Two successors are needed for send method", cg.getSuccNodeCount(sendNode) == 2);
-			    assertTrue("Two one successors are needed for get method", cg.getSuccNodeCount(getNode) == 2);
+			    assertTrue("Only one successor is needed for init method.", cg.getSuccNodeCount(initNode) == 1);
+			    assertTrue("Two successors are needed for send method.", cg.getSuccNodeCount(sendNode) == 2);
+			    assertTrue("Two one successors are needed for get method.", cg.getSuccNodeCount(getNode) == 2);
 			    
-			    assertTrue("Send method must to be called by JS code", (cg.getPredNodes(sendNode).next()).getMethod().getDeclaringClass().getClassLoader().getReference().equals(JavaScriptTypes.jsLoader));
-			    assertTrue("Get method must to be called by JS code", (cg.getPredNodes(getNode).next()).getMethod().getDeclaringClass().getClassLoader().getReference().equals(JavaScriptTypes.jsLoader));
+			    assertTrue("Send method must to be called by JS code.", (cg.getPredNodes(sendNode).next()).getMethod().getDeclaringClass().getClassLoader().getReference().equals(JavaScriptTypes.jsLoader));
+			    assertTrue("Get method must to be called by JS code.", (cg.getPredNodes(getNode).next()).getMethod().getDeclaringClass().getClassLoader().getReference().equals(JavaScriptTypes.jsLoader));
 			    
 			    IClass networkC = cg.getClassHierarchy().lookupClass(TypeReference.find(ClassLoaderReference.Application, "Lkr/ac/kaist/wala/hybridroid/test/reachability/NetworkClass"));
 			    IClass databaseC = cg.getClassHierarchy().lookupClass(TypeReference.find(ClassLoaderReference.Application, "Lkr/ac/kaist/wala/hybridroid/test/reachability/DatabaseClass"));
+			    
+			    IMethod initNWM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(networkC.getReference(), Selector.make("<init>()V")));
+			    CGNode initNWNode = cg.getNode(initNWM, Everywhere.EVERYWHERE);
+			    
+			    IMethod sendNWM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(networkC.getReference(), Selector.make("send(Ljava/lang/String;)V")));
+			    CGNode sendNWNode = cg.getNode(sendNWM, Everywhere.EVERYWHERE);
+			    
+			    IMethod initDBM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(databaseC.getReference(), Selector.make("<init>()V")));
+			    CGNode initDBNode = cg.getNode(initDBM, Everywhere.EVERYWHERE);
+			    
+			    IMethod getDBM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(databaseC.getReference(), Selector.make("getName()Ljava/lang/String;")));
+			    CGNode getDBNode = cg.getNode(getDBM, Everywhere.EVERYWHERE);
+			    
+			    for(Iterator<CGNode> iSucc = cg.getSuccNodes(sendNode); iSucc.hasNext(); ){
+			    	CGNode succ = iSucc.next();
+			    	
+			    	assertTrue("Successor of send method must be one of init network method or send network method.", (succ.equals(initNWNode) || succ.equals(sendNWNode)));			    	
+			    }
+			    
+			    for(Iterator<CGNode> iSucc = cg.getSuccNodes(getNode); iSucc.hasNext(); ){
+			    	CGNode succ = iSucc.next();
+			    	
+			    	assertTrue("Successor of get method must be one of init database method or get database method.", (succ.equals(initDBNode) || succ.equals(getDBNode)));			    	
+			    }
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void haveEdgeOnlyReachableMethod() throws ClassHierarchyException, IllegalArgumentException, IOException, CancelException{
+//		File[] tests = FileCollector.getAPKsInDir(HybriDroidTestRunner.getTestDir() + File.separator + TEST_DIR);
+		File[] tests = {new File(HybriDroidTestRunner.getTestDir() + File.separator + TEST_DIR + File.separator + "UnreachableMethodTest.apk")};
+		
+		for(File f : tests){
+			String testName = f.getName();
+			HybridCFGAnalysis cfgAnalysis = new HybridCFGAnalysis();
+			Pair<CallGraph, PointerAnalysis<InstanceKey>> p = cfgAnalysis.main(f.getCanonicalPath(), HybriDroidTestRunner.getLibPath());
+			CallGraph cg = p.fst;
+			PointerAnalysis<InstanceKey> pa = p.snd;
+
+			switch(testName){
+			case "UnreachableMethodTest.apk":
+			    IClass bridgeC = cg.getClassHierarchy().lookupClass(TypeReference.find(ClassLoaderReference.Application, "Lkr/ac/kaist/wala/hybridroid/test/unreachability/JSBridge"));
+			    
+			    IMethod initM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(bridgeC.getReference(), Selector.make("<init>()V")));
+			    CGNode initNode = cg.getNode(initM, Everywhere.EVERYWHERE);
+			    
+			    IMethod sendM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(bridgeC.getReference(), Selector.make("sendName(Ljava/lang/String;)V")));
+			    CGNode sendNode = cg.getNode(sendM, Everywhere.EVERYWHERE);
+			    
+			    IMethod getM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(bridgeC.getReference(), Selector.make("getName()Ljava/lang/String;")));
+			    CGNode getNode = cg.getNode(getM, Everywhere.EVERYWHERE);
+			    			   		    
+			    assertNotNull("No bridge init method.", initNode);
+			    assertNotNull("No bridge send method.", sendNode);
+			    assertNull("Get bridge method must be unreachable.", getNode);
+				
+			    
+			    assertTrue("Only one successor is needed for init method", cg.getSuccNodeCount(initNode) == 1);
+			    assertTrue("Two successors are needed for send method", cg.getSuccNodeCount(sendNode) == 2);
+			    
+			    
+			    assertTrue("Send method must to be called by JS code", (cg.getPredNodes(sendNode).next()).getMethod().getDeclaringClass().getClassLoader().getReference().equals(JavaScriptTypes.jsLoader));
+			    
+			    
+			    IClass networkC = cg.getClassHierarchy().lookupClass(TypeReference.find(ClassLoaderReference.Application, "Lkr/ac/kaist/wala/hybridroid/test/unreachability/NetworkClass"));
+			    IClass databaseC = cg.getClassHierarchy().lookupClass(TypeReference.find(ClassLoaderReference.Application, "Lkr/ac/kaist/wala/hybridroid/test/unreachability/DatabaseClass"));
+			    
+			    IMethod initNWM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(networkC.getReference(), Selector.make("<init>()V")));
+			    CGNode initNWNode = cg.getNode(initNWM, Everywhere.EVERYWHERE);
+			    
+			    IMethod sendNWM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(networkC.getReference(), Selector.make("send(Ljava/lang/String;)V")));
+			    CGNode sendNWNode = cg.getNode(sendNWM, Everywhere.EVERYWHERE);
+			    
+			    IMethod initDBM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(databaseC.getReference(), Selector.make("<init>()V")));
+			    CGNode initDBNode = cg.getNode(initDBM, Everywhere.EVERYWHERE);
+			    
+			    assertNull("Init Database method must be unreachable.", initDBNode);
+			    
+			    IMethod getDBM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(databaseC.getReference(), Selector.make("getName()Ljava/lang/String;")));
+			    CGNode getDBNode = cg.getNode(getDBM, Everywhere.EVERYWHERE);
+			    
+			    assertNull("Get Database method must be unreachable.", getDBNode);
+			    
+			    for(Iterator<CGNode> iSucc = cg.getSuccNodes(sendNode); iSucc.hasNext(); ){
+			    	CGNode succ = iSucc.next();
+			    	
+			    	assertTrue("Successor of send method must be one of init network method or send network method.", (succ.equals(initNWNode) || succ.equals(sendNWNode)));			    	
+			    }
+			    
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void haveEdgeFromBoth() throws ClassHierarchyException, IllegalArgumentException, IOException, CancelException{
+//		File[] tests = FileCollector.getAPKsInDir(HybriDroidTestRunner.getTestDir() + File.separator + TEST_DIR);
+		File[] tests = {new File(HybriDroidTestRunner.getTestDir() + File.separator + TEST_DIR + File.separator + "DualReachableMethodTest.apk")};
+		
+		for(File f : tests){
+			String testName = f.getName();
+			HybridCFGAnalysis cfgAnalysis = new HybridCFGAnalysis();
+			Pair<CallGraph, PointerAnalysis<InstanceKey>> p = cfgAnalysis.main(f.getCanonicalPath(), HybriDroidTestRunner.getLibPath());
+			CallGraph cg = p.fst;
+			PointerAnalysis<InstanceKey> pa = p.snd;
+			
+			switch(testName){
+			case "DualReachableMethodTest.apk":
+			    IClass bridgeC = cg.getClassHierarchy().lookupClass(TypeReference.find(ClassLoaderReference.Application, "Lkr/ac/kaist/wala/hybridroid/test/dualreachability/JSBridge"));
+			    
+			    IMethod initM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(bridgeC.getReference(), Selector.make("<init>()V")));
+			    CGNode initNode = cg.getNode(initM, Everywhere.EVERYWHERE);
+			    
+			    IMethod sendM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(bridgeC.getReference(), Selector.make("sendName(Ljava/lang/String;)V")));
+			    CGNode sendNode = cg.getNode(sendM, Everywhere.EVERYWHERE);
+			    
+			    IMethod getM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(bridgeC.getReference(), Selector.make("getName()Ljava/lang/String;")));
+			    CGNode getNode = cg.getNode(getM, Everywhere.EVERYWHERE);
+			    			   		    
+			    assertNotNull("No bridge init method.", initNode);
+			    assertNotNull("No bridge send method.", sendNode);
+			    assertNotNull("No bridge get method.", getNode);
+				
+			    assertTrue("Only one successor is needed for init method.", cg.getSuccNodeCount(initNode) == 1);
+			    assertTrue("Two successors are needed for send method.", cg.getSuccNodeCount(sendNode) == 2);
+			    assertTrue("Two one successors are needed for get method.", cg.getSuccNodeCount(getNode) == 2);
+			    
+			    assertTrue("Send method must to be called by JS code.", (cg.getPredNodes(sendNode).next()).getMethod().getDeclaringClass().getClassLoader().getReference().equals(JavaScriptTypes.jsLoader));
+			    assertTrue("Get method must have two predecessors.", cg.getPredNodeCount(getNode) == 2);
+			    
+			    boolean jspred = false;
+			    boolean javapred = false;
+			    
+			    for(Iterator<CGNode> iPred = cg.getPredNodes(getNode); iPred.hasNext(); ){
+			    	CGNode pred = iPred.next();
+			    			    	
+		    		if(pred.getMethod().getDeclaringClass().getClassLoader().getReference().equals(JavaScriptTypes.jsLoader))
+		    			jspred = true;
+		    		else if(pred.getMethod().getDeclaringClass().getClassLoader().getReference().equals(ClassLoaderReference.Application))
+		    			javapred = true;
+			    }
+			    
+			    assertTrue("Get method must have predecessors from both Java and JS.", jspred && javapred);
+			    
+			    IClass networkC = cg.getClassHierarchy().lookupClass(TypeReference.find(ClassLoaderReference.Application, "Lkr/ac/kaist/wala/hybridroid/test/dualreachability/NetworkClass"));
+			    IClass databaseC = cg.getClassHierarchy().lookupClass(TypeReference.find(ClassLoaderReference.Application, "Lkr/ac/kaist/wala/hybridroid/test/dualreachability/DatabaseClass"));
 			    
 			    IMethod initNWM = cg.getClassHierarchy().resolveMethod(MethodReference.findOrCreate(networkC.getReference(), Selector.make("<init>()V")));
 			    CGNode initNWNode = cg.getNode(initNWM, Everywhere.EVERYWHERE);
